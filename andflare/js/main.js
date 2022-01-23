93b1a4dec78a1b8f665b066aebbd0ae30461e2c0
Mile Stojanovic <milestojanovic3@hotmail.com>
2022-01-23 01:18:25 +0100

    fast commit

--- andflare/js/main.js
+++ andflare/js/main.js
@@ -2,3 +2,38 @@ var classes = ["bg1", "bg2", "bg3", "bg4"];
 var randomnumber = Math.floor(Math.random() * classes.length);
 
 $(".front-page").addClass(classes[randomnumber]);
+
+$(document).ready(function(){
+    $('.trending-slide').slick({
+        slidesToShow: 9,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                  slidesToShow: 4
+                }
+              },
+            {
+                breakpoint: 1400,
+                settings: {
+                  slidesToShow: 6
+                }
+              },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2
+              }
+            }
+          ]
+      });
+  });
